095b38e94bb8addb41511004e91f0fa215f81c40
aboveyunhai <35160613+aboveyunhai@users.noreply.github.com>
2020-07-29 15:40:58 -0400

    Update AppConstant.tsx

--- src/constants/AppConstant.tsx
+++ src/constants/AppConstant.tsx
@@ -6,7 +6,7 @@ const baseColor = "#21252b"
 const baseDarkColor = "#1d2026";
 const componentColor = "#282c34";
 const opa80 = 'CC', opa60 = '99', opa40 = '66', opa20 = '33';
-const grey = '#abb2bf';
+const grey = '#C4C4C4';
 const highlightBlue = '#58afef';
 const highlightBlGr = '#56b6c2';
 const highlightGreen = '#73c990';
@@ -45,10 +45,35 @@ const { width: SCREEN_W, height: SCREEN_H } = Dimensions.get('window');
 /* circle size to short edge */
 const CIRCLE_S = SCREEN_W < SCREEN_H ? SCREEN_W * 0.7 : SCREEN_H * 0.7;
 
-const AppComponentSize = {
+const NAVI_HEADER_HEIGHT = 45;
+const TABBAR_HEIGHT = 50;
+const GRAPH_HEIGHT = 200;
+
+const AppCompSize = {
   SCREEN_W,
   SCREEN_H,
-  CIRCLE_S
+  CIRCLE_S,
+  NAVI_HEADER_HEIGHT,
+  TABBAR_HEIGHT,
+  GRAPH_HEIGHT,
 }
 
-export { AppColor, AppComponentSize };
+const AppScreen = {
+  MAIN: 'Main',
+  HOME: 'Home',
+  STATISTICS: 'Statistics',
+  SETTINGS: 'Settings',
+  RECENT: 'Recent',
+  OVERALL: 'Overall'
+}
+
+const AppFont = {
+  Oxanium: {
+    regular: 'Oxanium-Regular',
+    light: 'Oxanium-Light',
+    extraLight: 'Oxanium-ExtraLight',
+    bold: 'Oxanium-Bold'
+  }
+}
+
+export { AppColor, AppCompSize, AppScreen, AppFont };
